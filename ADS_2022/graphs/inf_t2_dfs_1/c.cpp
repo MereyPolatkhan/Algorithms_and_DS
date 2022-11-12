fff65fa99fe1dbd14ba706b04f5a1d0c840e51bb
Merey Polatkhan <m_polatkhan@kbtu.kz>
2022-11-12 09:21:55 +0600

    labs

--- ADS_2022/graphs/inf_t2_dfs_1/c.cpp
+++ ADS_2022/graphs/inf_t2_dfs_1/c.cpp
@@ -1,17 +1,40 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
 
 using namespace std;
 
 vector<int> g[1000];
-int used[1000];
+bool used[1000];
 
+bool is_cycle(int v, int parent) {
+    used[v] = true;
+    for (auto sosedy: g[v]) {
+        if (used[sosedy] == false){ 
+            if (is_cycle(sosedy, v) == true) {
+                return true;
+            }
+        }
+        else {
+            if (sosedy != parent) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool is_connected(int n) {
+    for (int i = 0; i < n; i++) {
+        if (used[i] == false) {
+            return false;
+        }
+    }
+    return true;
+}
 
 
 int main() {
     int n;
     cin >> n;
-
-    
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -22,7 +45,13 @@ int main() {
             }
         }
     }
- 
 
+
+    if (is_cycle(0, -1) == false && is_connected(n) == true) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
     return 0;
 }
